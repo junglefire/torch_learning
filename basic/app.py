e8dbe6d143d4c48748096db15e19a08d8d4dad1f
王征 <alex.wz@alibaba-inc.com>
2020-12-18 17:51:09 +0800

    basic done

--- basic/app.py
+++ basic/app.py
@@ -10,12 +10,12 @@ log.basicConfig(format='[%(asctime)s][%(levelname)s] %(message)s', level=log.INF
 @click.command()
 @click.option("--cmd", help="show: 显示数据集图形; train: 训练模型", type=str)
 def main(cmd):
-    if cmd == 'show':
-    	__dataset_show()
-    elif cmd == 'train':
-    	__model_train()
-    else:
-    	log.error("invalid command!")
+	if cmd == 'show':
+		__dataset_show()
+	elif cmd == 'train':
+		__model_train()
+	else:
+		log.error("invalid command!")
 
 # 生成数据集
 def __gen_dataset():
@@ -48,19 +48,29 @@ def __model_train():
 	learning_rate = 0.0001
 	# 模型训练
 	for i in range(1000):
-    	predictions = a.expand_as(x_train) * x_train + b.expand_as(x_train)
-    	# 将所有训练数据代入模型ax+b，计算每个的预测值。这里的x_train和predictions都是（90，1）的张量。
-    	# expand_as的作用是将a,b扩充维度到和x_train一致
-    	loss = torch.mean((predictions - y_train) ** 2)
-    	log.info('loss:', loss)
-    	loss.backward() #对损失函数进行梯度反传
-    	# 利用上一步计算中得到的梯度信息更新参数中的data数值
-    	a.data.add_(- learning_rate * a.grad.data)
-    	b.data.add_(- learning_rate * b.grad.data)
-    	# 清空存储在变量a、b中的梯度信息，以免在backward的过程中反复不停地累加
-    	a.grad.data.zero_() #清空a的梯度数值
-    	b.grad.data.zero_() #清空b的梯度数值
+		predictions = a.expand_as(x_train) * x_train + b.expand_as(x_train)
+		# 将所有训练数据代入模型ax+b，计算每个的预测值。这里的x_train和predictions都是（90，1）的张量。
+		# expand_as的作用是将a,b扩充维度到和x_train一致
+		loss = torch.mean((predictions - y_train) ** 2)
+		print(loss)
+		loss.backward() #对损失函数进行梯度反传
+		# 利用上一步计算中得到的梯度信息更新参数中的data数值
+		a.data.add_(- learning_rate * a.grad.data)
+		b.data.add_(- learning_rate * b.grad.data)
+		# 清空存储在变量a、b中的梯度信息，以免在backward的过程中反复不停地累加
+		a.grad.data.zero_() #清空a的梯度数值
+		b.grad.data.zero_() #清空b的梯度数值
+	# 比较原始数据集和拟合的直线
+	x_data = x_train.data.numpy()
+	plt.figure(figsize = (8, 6))
+	xplot, = plt.plot(x_data, y_train.data.numpy(), 'o') 
+	yplot, = plt.plot(x_data, a.data.numpy() * x_data +b.data.numpy())
+	plt.xlabel('X')
+	plt.ylabel('Y') 
+	str1 = str(a.data.numpy()[0]) + 'x +' + str(b.data.numpy()[0])      #将拟合直线的参数a、b显示出来
+	plt.legend([xplot, yplot],['Data', str1])      #绘制图例
+	plt.show() 
 
 
 if __name__ == '__main__':
-    main()
+	main()
